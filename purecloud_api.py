24c001c76e1f5bdb33ead8d0f936c74a8098ff57
Gustavo Batista <gbatistamartins@gmail.com>
2025-04-08 09:00:06 -0300

    Update purecloud_api.py
    Adding header that includes the license.

--- purecloud_api.py
+++ purecloud_api.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Official repo https://github.com/gstvbatista/gc_get_user_time
+#
+# Copyright (C) [Year] [Your Name or Your Organization]
+#
+# This file is part of https://github.com/gstvbatista/gc_get_user_time.
+#
+# This software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import base64
 import logging
